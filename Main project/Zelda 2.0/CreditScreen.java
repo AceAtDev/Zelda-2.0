5a81b76230e6f25a8841de599c6ac908aa43eab2
Ahsen Syed <116108200+1syedahs@users.noreply.github.com>
2023-01-21 00:56:58 -0500

    UI Finished?
    -  I Finished the Code for the UI with Clickable Buttons
    - However, I get an OUTOFMEMORY ERROR
    - Code Works but I can't put all of the buttons into the game at one time
    - Need Ace to make Code more efficient for space management
    - For now, Game Works if You skip the title screen (Right -Click on gameplay tab)

---
+++ Main project/Zelda 2.0/CreditScreen.java
@@ -0,0 +1,30 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class CreditScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CreditScreen extends World
+
+{
+ uiButtons menuButton = new uiButtons("Button Image.png",new TitleScreen());
+    /**
+     * Constructor for objects of class CreditScreen
+     */
+    public CreditScreen()
+    {
+        super(680,480,1);
+        prepareCreditScreen();
+    }
+    public void prepareCreditScreen(){
+    CreditScreenLetters creditFont = new CreditScreenLetters();
+    addObject(creditFont,340,240);
+    addObject(menuButton,90,440);
+    }
+    public void act(){
+    }
+
+   
+}
---
+++ Main project/Zelda 2.0/uiButtons.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class uiButtons here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class uiButtons extends Actor
+{
+    private World screenChange;
+    
+    public uiButtons(String Image, World screenChange){
+        setImage(new GreenfootImage(Image));
+        this.screenChange = screenChange;
+    }
+    
+    public void act()
+    {
+        if(Greenfoot.mouseClicked(this)){ // If Object Clicked, Will 
+            Greenfoot.setWorld(this.screenChange);
+        }
+    }
+}
